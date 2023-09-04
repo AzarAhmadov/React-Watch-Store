b5029c28be8abf2b4bd551ef22a6e0a18a1b8464
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-09-04 21:08:51 +0400

    .

--- src/app/features/basket/getBasket.js
+++ src/app/features/basket/getBasket.js
@@ -1,10 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 
+const getBasketFromLocalStorage = () => {
+  const basketData = localStorage.getItem('basket');
+  return basketData ? JSON.parse(basketData) : { items: [] };
+};
+
 const basketSlice = createSlice({
   name: 'basket',
-  initialState: {
-    items: [],
-  },
+  initialState: getBasketFromLocalStorage(),
   reducers: {
     addToBasket: (state, action) => {
       const existingItem = state.items.find(item => item.id === action.payload.id);
@@ -13,15 +16,18 @@ const basketSlice = createSlice({
       } else {
         state.items.push({ ...action.payload, quantity: 1 });
       }
+      localStorage.setItem('basket', JSON.stringify(state));
     },
     removeFromBasket: (state, action) => {
       state.items = state.items.filter(item => item.id !== action.payload.id);
+      localStorage.setItem('basket', JSON.stringify(state));
     },
     updateQuantity: (state, action) => {
       const { id, quantity } = action.payload;
       const product = state.items.find(item => item.id === id);
       if (product) {
         product.quantity = quantity;
+        localStorage.setItem('basket', JSON.stringify(state));
       }
     },
   },
--- src/app/features/favorite/getFavorite.js
+++ src/app/features/favorite/getFavorite.js
@@ -1,36 +1,52 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const getFavorite = createSlice({
-    name: 'product',
-    initialState: {
-        favorites: [],
-        selectedProduct: [],
+const getFavoritesFromLocalStorage = () => {
+  const favoritesData = localStorage.getItem('favorites');
+  const selectedProductData = localStorage.getItem('selectedProduct');
+  
+  const favorites = favoritesData ? JSON.parse(favoritesData) : [];
+  const selectedProduct = selectedProductData ? JSON.parse(selectedProductData) : [];
+
+  return { favorites, selectedProduct };
+};
+
+const saveFavoritesToLocalStorage = (favorites) => {
+  localStorage.setItem('favorites', JSON.stringify(favorites));
+};
+
+const saveSelectedProductToLocalStorage = (selectedProduct) => {
+  localStorage.setItem('selectedProduct', JSON.stringify(selectedProduct));
+};
+
+const favoriteSlice = createSlice({
+  name: 'product',
+  initialState: getFavoritesFromLocalStorage(),
+  reducers: {
+    toggleFavorite: (state, action) => {
+      const id = action.payload;
+      if (state.favorites.includes(id)) {
+        state.favorites = state.favorites.filter((favoriteId) => favoriteId !== id);
+      } else {
+        state.favorites = [...state.favorites, id];
+      }
+      saveFavoritesToLocalStorage(state.favorites);
     },
-    reducers: {
-        toggleFavorite: (state, action) => {
-            const id = action.payload;
-            if (state.favorites.includes(id)) {
-                state.favorites = state.favorites.filter((favoriteId) => favoriteId !== id);
-            } else {
-                state.favorites = [...state.favorites, id];
-            }
-        },
-        setSelectedProduct: (state, action) => {
-            const existingProduct = state.selectedProduct.find((product) => product.id === action.payload.id);
-            if (existingProduct) {
-                state.selectedProduct = state.selectedProduct.filter((product) => product.id !== action.payload.id);
-            } else {
-                state.selectedProduct.push({
-                    id: action.payload.id,
-                    title: action.payload.title,
-                    price: action.payload.price,
-                    img: action.payload.img,
-                });
-            }
-        },
+    setSelectedProduct: (state, action) => {
+      const existingProduct = state.selectedProduct.find((product) => product.id === action.payload.id);
+      if (existingProduct) {
+        state.selectedProduct = state.selectedProduct.filter((product) => product.id !== action.payload.id);
+      } else {
+        state.selectedProduct.push({
+          id: action.payload.id,
+          title: action.payload.title,
+          price: action.payload.price,
+          img: action.payload.img,
+        });
+      }
+      saveSelectedProductToLocalStorage(state.selectedProduct);
     },
+  },
 });
 
-export const { toggleFavorite, setSelectedProduct } = getFavorite.actions;
-
-export default getFavorite.reducer;
+export const { toggleFavorite, setSelectedProduct } = favoriteSlice.actions;
+export default favoriteSlice.reducer;
