d92532af4e5c69cddae2eb91479e2c27533f8de9
AzarAhmadov <82292818+AzarAhmadov@users.noreply.github.com>
2023-09-05 23:46:58 +0400

    .

--- src/layout/Loading.jsx
+++ src/layout/Loading.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Loading = () => {
     return (
         <div className='loading'>
-           loading...
+            <span class="loader"></span>
         </div>
     )
 }
